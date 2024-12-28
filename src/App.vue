e2ac6f56567fdc6e2f34463e919d7b830b5cc408
Victor Chiaka <chiakavictor205@gmail.com>
2024-12-29 00:17:06 +0100

    ft: implemented landing page

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Navbar from "@/components/Navbar.vue";
+import Main from "@/components/Main.vue";
+</script>
+
+<template>
+  <Navbar />
+  <Main />
+</template>
+
+<style scoped lang="scss"></style>
